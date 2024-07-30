096f457e170de87deea887b9e6bac9c41a43b9d1
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-07-29 21:20:26 -0300

    feat: adding new styles for components

--- src/components/UniversalButton/style.js
+++ src/components/UniversalButton/style.js
@@ -8,5 +8,10 @@ export const Button = styled.button`
   border: none;
   background-color: ${(props) => props.color || "black"};
   color: white;
-  transition: background-color 0.5s ease;
+  transition: background-color 0.5s ease, box-shadow 0.3s ease;
+  padding: 2px;
+
+  &:hover {
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.3);
+  }
 `;
--- src/components/UniversalTitle/style.js
+++ src/components/UniversalTitle/style.js
@@ -1,11 +1,10 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-    font-size: 2rem;
-    color: ${(props) => props.color || "#333"};
-    margin: 20px 0;
-    text-align: center;
-    transition: color 0.5s ease;
+  font-size: 2.2rem;
+  color: ${(props) => props.color || "black"};
+  margin: 10px 0;
+  font-weight: 700;
+  text-align: center;
+  transition: color 0.5s ease;
 `;
-   
-    

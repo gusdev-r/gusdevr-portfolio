8604e0d5dc7932ecd320966b2e46f84551c88f9f
mateusvincenzi <mateusvincenzids@gmail.com>
2024-08-03 19:12:26 -0300

    unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Singin from "./pages/signin/signin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Singin />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);

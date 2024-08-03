e069a6b27722ccd1155eeac78400fd458044b021
mateusvincenzi <mateusvincenzids@gmail.com>
2024-08-03 19:05:21 -0300

    add feat/signin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Singin from "./pages/signin/signin";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Singin />
+  </React.StrictMode>
+);

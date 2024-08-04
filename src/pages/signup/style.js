808294c96cf7112a1296a531b003d34bbe07756f
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-04 18:59:08 -0300

    refactor: removing unnecessary files

--- src/pages/signup/style.js
+++ src/pages/signup/style.js
@@ -16,7 +16,6 @@ export const GlobalStyle = createGlobalStyle`
 `;
 
 export const Container = styled.div`
-  /* height: 100vh; */
   display: flex;
   justify-content: center;
   align-items: center;

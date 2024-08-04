120b2221f4cd347c2d2ea2b6865bf5e240d6dd2d
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-04 17:31:42 -0300

    feat: header component

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,105 +1,85 @@
-import {
-  HeaderComp,
-  GlobalStyle,
-  LogoBox,
-  Logo,
-  SingBtns,
-  ContainerElements,
-  ContainerButtons,
-  Element,
-
-  // -------
-  NavbarCollapse,
-  NavbarNav,
-  NavItem,
-  NavLink,
-  Dropdown,
-  DropdownMenu,
-  DropdownItem,
-  FormInline,
-  FormControl,
-  Button,
-  TestContainer,
-} from "./style";
+import { useState } from "react";
+import icon_search from "../../assets/images/search-icon.png";
 import logo from "../../assets/images/snug.png";
+import {
+  Button,
+  ContainerButtons,
+  ContainerElements,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  Element,
+  FormControl,
+  FormInline,
+  HeaderComp,
+  Icon,
+  Logo,
+  LogoBox,
+} from "./style";
 
 export const Header = () => {
+  const [searchContent, setSearchContent] = useState("");
+
+  const handleSearch = () => {
+    // logic to work with the search state if this is used by you
+    console.log(searchContent);
+  };
+
+  const handleChange = (event) => {
+    setSearchContent(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
-    <>
-      <GlobalStyle />
-      <HeaderComp>
-        <LogoBox>
-          <Logo src={logo} alt="logo" />
-        </LogoBox>
-        <ContainerElements>
-          <FormInline>
-            <FormControl
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <Button
-              type="button"
-              onClick={() => {}}
-              bgColor="#303030"
-              color="white"
-            >
-              Search
-            </Button>
-          </FormInline>
-          <Element>Link 1</Element>
-          <Element>Link 2</Element>
-          <Element>Link 3</Element>
-          <ContainerButtons>
-            <Button
-              type="button"
-              onClick={() => {}}
-              bgColor="grey"
-              color="white"
-            >
-              Cadastrar
-            </Button>
-            <Button
-              type="button"
-              onClick={() => {}}
-              bgColor="white"
-              color="black"
-            >
-              Login
-            </Button>
-          </ContainerButtons>
-        </ContainerElements>
-      </HeaderComp>
-      {/* ---- */}
-      {/* <TestContainer>
-        <NavbarCollapse>
-          <NavbarNav>
-            <NavItem className="nav-item active">
-              <NavLink href="#">Home</NavLink>
-            </NavItem>
-            <NavItem className="nav-item">
-              <NavLink href="#">Link</NavLink>
-            </NavItem>
-            <Dropdown className="nav-item dropdown">
-              <NavLink
-                href="#"
-                className="nav-link dropdown-toggle"
-                role="button"
-              >
-                Dropdown
-              </NavLink> */}
-      {/* <DropdownMenu>
-                <DropdownItem href="#">Action</DropdownItem>
-                <DropdownItem href="#">Another action</DropdownItem>
-                <DropdownItem href="#">Something else here</DropdownItem>
-              </DropdownMenu> */}
-      {/* </Dropdown> */}
-      {/* <NavItem className="nav-item disabled">
-              <NavLink href="#">Disabled</NavLink>
-            </NavItem> */}
-      {/* </NavbarNav>
-        </NavbarCollapse>
-      </TestContainer> */}
-    </>
+    <HeaderComp>
+      <LogoBox>
+        <Logo src={logo} alt="logo" />
+      </LogoBox>
+      <ContainerElements>
+        <FormInline>
+          <FormControl
+            type="search"
+            placeholder="Buscar"
+            aria-label="Search"
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+          />
+          <Icon onClick={handleSearch} src={icon_search} alt="search-icon" />
+        </FormInline>
+        <Element>Link 1</Element>
+        <Element>Link 2</Element>
+        <Dropdown>
+          <Element role="button">Options</Element>
+          <DropdownMenu>
+            <DropdownItem href="#">Action 1</DropdownItem>
+            <DropdownItem href="#">Action 2</DropdownItem>
+            <DropdownItem href="#">Action 3</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <ContainerButtons>
+          <Button
+            type="button"
+            onClick={() => {}}
+            bgColorHover="#dbdbdb"
+            colorHover="#222222"
+          >
+            Cadastrar
+          </Button>
+          <Button
+            type="button"
+            onClick={() => {}}
+            bgColorHover="#dbdbdb"
+            colorHover="#222222"
+          >
+            Login
+          </Button>
+        </ContainerButtons>
+      </ContainerElements>
+    </HeaderComp>
   );
 };

d55e2348f62e3a2266ccc36cd92f6bc576d8cfe0
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-05 01:01:49 -0300

    feat: adding correct layout for footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -9,29 +9,38 @@ import {
   PolicyLinks,
   PolicyLink,
   Copyright,
+  GlobalStyle,
+  LinksContainer,
 } from "./style";
 
 const Footer = () => {
   return (
-    <FooterContainer>
-      <LogoBox>
-        <Logo src={logo} alt="Logo" />
-      </LogoBox>
-      <MainLinks>
-        <MainLink href="#">Link One</MainLink>
-        <MainLink href="#">Link Two</MainLink>
-        <MainLink href="#">Link Three</MainLink>
-        <MainLink href="#">Link Four</MainLink>
-        <MainLink href="#">Link Five</MainLink>
-      </MainLinks>
-      <Divider />
-      <PolicyLinks>
-        <PolicyLink href="#">Privacy Policy</PolicyLink>
-        <PolicyLink href="#">Terms of Service</PolicyLink>
-        <PolicyLink href="#">Cookie Settings</PolicyLink>
-      </PolicyLinks>
-      <Copyright>&copy; 2024 Your Company. All rights reserved.</Copyright>
-    </FooterContainer>
+    <>
+      <GlobalStyle />
+
+      <FooterContainer>
+        <LogoBox>
+          <Logo src={logo} alt="Logo" />
+        </LogoBox>
+
+        <LinksContainer>
+          <MainLinks>
+            <MainLink href="#">Link One</MainLink>
+            <MainLink href="#">Link Two</MainLink>
+            <MainLink href="#">Link Three</MainLink>
+            <MainLink href="#">Link Four</MainLink>
+            <MainLink href="#">Link Five</MainLink>
+          </MainLinks>
+          <Divider />
+          <PolicyLinks>
+            <PolicyLink href="#">Privacy Policy</PolicyLink>
+            <PolicyLink href="#">Terms of Service</PolicyLink>
+            <PolicyLink href="#">Cookie Settings</PolicyLink>
+          </PolicyLinks>
+          <Copyright>&copy; 2024 Your Company. All rights reserved.</Copyright>
+        </LinksContainer>
+      </FooterContainer>
+    </>
   );
 };
 
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,57 +1,67 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 export const FooterContainer = styled.footer`
-  height: 20rem;
+  height: 7.8rem;
   width: 100%;
-  position: relative;
   background-color: hsl(0, 0%, 9%);
   display: flex;
   align-items: center;
-  //   top: 0;
-  //   right: 0;
-  //   background-color: hsl(0, 0%, 9%);
-  //   align-itens: center;
-  //   padding: 20px 0;
-  //   position: fixed;
-  //   width: 100%;
-  //   height: 6.8rem;
-  //   border-top: 1px solid #dee2e6;
+  justify-content: space-between;
+  padding: 20px 0;
 `;
 
 export const LogoBox = styled.div`
-  margin-bottom: 20px;
+  height: 100%;
+  max-width: 18rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 export const Logo = styled.img`
-  height: 100px;
-  background-color: gray;
+  height: 100%;
+  max-width: 19rem;
+  /* background-color: red; */
+`;
+
+export const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  align-items: center;
+  /* background-color: yellow; */
 `;
 
 export const MainLinks = styled.div`
   display: flex;
   justify-content: center;
-  gap: 50px;
-  margin-bottom: 20px;
-  margin-left: 65.5rem;
-  background-color: gray;
+  gap: 4rem;
+  padding: 4px;
 `;
 
 export const MainLink = styled.a`
   color: white;
   text-decoration: none;
   font-size: 14px;
-
+  padding: 4px;
   &:hover {
     text-decoration: underline;
   }
+  /* background-color: blue; */
 `;
 
 export const Divider = styled.div`
-  width: 35%;
-  height: 1px;
-  background-color: gray;
-  margin: 20px auto;
-  margin-left: 71rem;
+  width: 70%;
+  height: 2px;
+  margin: 10px;
+  background-color: #fff;
 `;
 
 export const PolicyLinks = styled.div`
@@ -59,15 +69,16 @@ export const PolicyLinks = styled.div`
   justify-content: center;
   gap: 70px;
   margin-bottom: 20px;
-  background-color: gray;
-  margin-left: 66rem;
+  /* background-color: gray; */
+  padding: 2px;
 `;
 
 export const PolicyLink = styled.a`
   color: white;
   text-decoration: none;
   font-size: 12px;
-
+  /* background-color: purple; */
+  padding: 2px;
   &:hover {
     text-decoration: underline;
   }
@@ -76,6 +87,8 @@ export const PolicyLink = styled.a`
 export const Copyright = styled.div`
   font-size: 12px;
   color: white;
-  margin-left: 85rem;
-  background-color: gray;
+  text-align: center;
+  /* background-color: gray; */
+  margin-top: 20px;
+  width: 100%;
 `;

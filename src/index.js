27aec2ec99e234369b93df08ae837bd824bd6e5b
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-06 23:31:11 -0300

    feat: adding global style base

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Singup from "./pages/signup/signup";
+import { GlobalStyle } from "./styles/mainStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <Singup />
+  </React.StrictMode>
+);

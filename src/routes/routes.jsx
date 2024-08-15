ad4a30073fdf0025b71b4d733ca2b86dbf61c241
mateusvincenzi <mateusvincenzids@gmail.com>
2024-08-15 18:31:46 -0300

    feat: start globalstyled

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,14 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Singup from "../pages/signup/signup";
 import Singin from "../pages/signin/signin";
 import Layout from "../components/baseLayout/baseLayout";
+import GlobalStyled from "../styles/globalstyle";
 
 export default function RoutesApp() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/register" element={<Singup />} />
-        <Route path="/login" element={<Singin />} />
-      </Route>
-    </Routes>
+    <>
+    <GlobalStyled/>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/register" element={<Singup />} />
+          <Route path="/login" element={<Singin />} />
+        </Route>
+      </Routes>
+    </>
   );
 }

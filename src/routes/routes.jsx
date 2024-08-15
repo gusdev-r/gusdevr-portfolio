575eda0790e46de1f7fb2584c4ec96e2ffcab813
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-14 23:56:04 -0300

    feat: creating routes path file

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,15 @@
 import { Route, Routes } from "react-router-dom";
+import Singup from "../pages/signup/signup";
+import Singin from "../pages/signin/signin";
+import Layout from "../components/baseLayout/baseLayout";
 
 export default function RoutesApp() {
   return (
     <Routes>
-      <Route></Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="/register" element={<Singup />} />
+        <Route path="/login" element={<Singin />} />
+      </Route>
     </Routes>
   );
 }

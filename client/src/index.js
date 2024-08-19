b3a9f950151a45bd5d8dfeedf3481723f77036b8
Mateus Vincenzi <mateusvincenzids@gmail.com>
2024-08-18 23:35:57 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Singin from "./pages/signin/signin";
 import GlobalStyled from "./styles/globalstyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalStyled />
-    <Singin />
   </React.StrictMode>
 );

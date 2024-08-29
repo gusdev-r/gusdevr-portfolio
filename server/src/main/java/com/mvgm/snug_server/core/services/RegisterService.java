95b6f49973831cb3530692ad0415ede71a3dc0c7
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-28 23:19:24 -0300

    feat: base register service

---
+++ server/src/main/java/com/mvgm/snug_server/core/services/RegisterService.java
@@ -0,0 +1,60 @@
+package com.mvgm.snug_server.core.services;
+
+import com.mvgm.snug_server.core.domain.entity.Token;
+import com.mvgm.snug_server.core.domain.entity.User;
+import com.mvgm.snug_server.core.usecases.TokenUseCase;
+import com.mvgm.snug_server.infra.persistence.UserRepositoryImp;
+import com.mvgm.snug_server.utils.Constants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RegisterService {
+
+    private final UserRepositoryImp userRepo;
+    private final UserValidationService userValidation;
+    private final TokenUseCase confirmToken;
+    private final EmailService emailService;
+
+    public String generateTokenFromUser (User userDto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodedPassword = encoder.encode(userDto.getPassword());
+        if(!encoder.matches(userDto.getPassword(), encodedPassword)) {
+            throw new IllegalArgumentException("Invalid password");
+        }
+
+        User user = new User();
+        user.setEmail(userDto.getEmail());
+        user.setUsername(userDto.getUsername());
+        user.setPassword(encodedPassword);
+        userRepo.save(user);
+
+        String tokenToConfirm = UUID.randomUUID().toString();
+        Token token = Token.builder()
+                .token(tokenToConfirm).user(user)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(20))
+                .build();
+
+        confirmToken.save(token);
+        return tokenToConfirm;
+    }
+
+    public String registerUser (User userDto) {
+        userValidation.emailIsRegistered(userDto.getEmail());
+        userValidation.usernameIsRegistered(userDto.getUsername());
+        userValidation.validateEmail(userDto.getEmail());
+        userValidation.validatePassword(userDto.getPassword());
+
+        User user = User.builder().build();
+
+        String token = generateTokenFromUser(userToRegister);
+        String link = Constants.TOKEN_URL + token;
+        emailService.send(user.getEmail(), link);
+    }
+}

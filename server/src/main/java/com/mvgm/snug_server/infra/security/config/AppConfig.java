dd2f745e3f8fcf23d4c9c9fc8f8d290872af97ed
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-28 23:17:37 -0300

    feat: adding base configs for spring sec

---
+++ server/src/main/java/com/mvgm/snug_server/infra/security/config/AppConfig.java
@@ -0,0 +1,46 @@
+package com.mvgm.snug_server.infra.security.config;
+
+import com.mvgm.snug_server.infra.persistence.UserRepositoryImp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@Configuration
+public class AppConfig {
+
+    private final UserRepositoryImp userRepo;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> (UserDetails) userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration conf) throws Exception {
+        return conf.getAuthenticationManager();
+    }
+
+}

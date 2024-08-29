840f52345d7562761309eec49db178603b5e3426
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-08-28 23:11:44 -0300

    feat: utils files

---
+++ server/src/main/java/com/mvgm/snug_server/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.mvgm.snug_server.utils;
+
+public class Constants {
+    public static final String BASE_URL = "/v1/api";
+    public static final String TOKEN_URL = "http://localhost:8082/api/auth/confirm/token?token=";
+}

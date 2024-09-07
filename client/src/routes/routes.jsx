a28167f069abe242ca74af830cfa70a8cbcd1656
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-09-06 21:26:26 -0300

    feat: route for each page

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,15 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "../components/baseLayout/baseLayout";
-import Singin from "../pages/signin/signin";
-import Singup from "../pages/signup/signup";
+import { Route, Routes } from 'react-router-dom'
+import Layout from '../components/baseLayout/baseLayout'
+import Singin from '../pages/signin/signin'
+import Singup from '../pages/signup/signup'
+import Home from '../pages/Home/home'
 
-export default function RoutesApp() {
+export default function RoutesMainApp() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Home />}>
         <Route path="/register" element={<Singup />} />
         <Route path="/login" element={<Singin />} />
       </Route>
     </Routes>
-  );
+  )
 }

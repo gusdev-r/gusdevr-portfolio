245aca8a0841eb1750d608461143f8b2bf7bb897
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-09-06 23:35:46 -0300

    fix: correcting route position

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from '../components/baseLayout/baseLayout'
-import Singin from '../pages/signin/signin'
-import Singup from '../pages/signup/signup'
+import Signup from '../pages/signup/signup'
+import Signin from '../pages/signin/signin'
 import Home from '../pages/Home/home'
 
 export default function RoutesMainApp() {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="/register" element={<Singup />} />
-        <Route path="/login" element={<Singin />} />
+      <Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="register" element={<Signup />} />
+        <Route path="login" element={<Signin />} />
       </Route>
     </Routes>
   )

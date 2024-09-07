db8b936e2bb1459fe1d2e832ae5ab9ac58c67b42
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-09-06 22:15:21 -0300

    feat: adding route at dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import GlobalStyled from "./styles/globalstyle";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GlobalStyled from './styles/globalstyle'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import RoutesMainApp from './routes/routes'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <GlobalStyled />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<RoutesMainApp />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)

9460f83f15d2a844003ea6befaa2c90c0a31df82
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-04-23 08:25:12 -0300

    feat: new working projects

--- client/src/pages/projects/components/projectBox/box.jsx
+++ client/src/pages/projects/components/projectBox/box.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react'
-import { FaGithub } from 'react-icons/fa6'
-import { MdCorporateFare } from 'react-icons/md'
-import styled from 'styled-components'
-import { StyledSmallSubTitle } from '../../../../components/mainTexts/descText/desc'
-import { StyledMediumSubTitle } from '../../../../components/mainTexts/mediumBoldText/medium'
-import breakpoint from '../../../../styles/breakpoints'
+import React, { useState } from "react";
+import { FaGithub } from "react-icons/fa6";
+import { MdCorporateFare } from "react-icons/md";
+import styled from "styled-components";
+import { StyledSmallSubTitle } from "../../../../components/mainTexts/descText/desc";
+import { StyledMediumSubTitle } from "../../../../components/mainTexts/mediumBoldText/medium";
+import breakpoint from "../../../../styles/breakpoints";
 
 export const ProjBox = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  height: 18rem;
+  height: 22rem;
   width: 42rem;
   align-items: center;
   border-radius: 2rem;
-  transition: all 0.5s ease;
+  transition: all 1.2s ease;
   border: 2px solid var(--grey1);
   cursor: pointer;
 
@@ -34,13 +34,15 @@ export const ProjBox = styled.div`
   }
 
   &:hover {
-    box-shadow: 0 0 25px 0px rgba(66, 66, 66, 0.546);
+    box-shadow: 0 0 0.8rem 0px rgba(66, 66, 66, 0.546);
+    border: 2px solid var(--grey2);
     svg {
       color: var(--grey7);
     }
   }
   @media ${breakpoint.bg1} {
     width: 34rem;
+    height: 24rem;
   }
   @media ${breakpoint.sm0} {
     height: 16rem;
@@ -54,13 +56,13 @@ export const ProjBox = styled.div`
       font-size: 1.4rem;
     }
   }
-`
+`;
 
 const GeneralBoxProject = ({
   title,
   subtitle,
   sideTitle,
-  sideSubtile,
+  sideSubtitle,
   i1,
   i2,
   i3,
@@ -69,37 +71,50 @@ const GeneralBoxProject = ({
   i6,
   projectLink,
 }) => {
-  const [changeSide, setChangeSide] = useState(false)
+  const [changeSide, setChangeSide] = useState(false);
 
   const toggleChangeSide = () => {
-    setChangeSide(!changeSide)
+    setChangeSide(!changeSide);
 
     setTimeout(() => {
-      setChangeSide(false)
-    }, 10000)
-  }
+      setChangeSide(false);
+    }, 10000);
+  };
+
+  const verifyContent = (content, substitute) => {
+    if (content) return content;
+    return substitute;
+  };
 
   return (
     <ProjBox onClick={toggleChangeSide}>
       <div id="handleSideIcons">
-        {sideTitle === 'Projeto no Github' && changeSide ? (
+        {verifyContent(sideTitle, "Ambiente corporativo") ===
+          "Projeto no Github" && changeSide ? (
           <FaGithub />
         ) : (
           <>{changeSide ? <MdCorporateFare /> : <></>}</>
         )}
       </div>
       <StyledMediumSubTitle
+        fontSize="1.1rem"
+        paddingBottom="0.65rem"
         color="var(--grey6)"
         width="80%"
-        textAlign={changeSide ? 'center' : 'left'}
+        textAlign={changeSide ? "center" : "left"}
       >
-        {changeSide ? sideTitle : title}
+        {changeSide ? verifyContent(sideTitle, "Ambiente corporativo") : title}
       </StyledMediumSubTitle>
       <StyledSmallSubTitle
         width="80%"
-        textAlign={changeSide ? 'center' : 'left'}
+        textAlign={changeSide ? "center" : "left"}
       >
-        {changeSide ? sideSubtile : subtitle}
+        {changeSide
+          ? verifyContent(
+              sideSubtitle,
+              "Este projeto foi desenvolvido em ambiente comporativo, não é possível compartilhar.",
+            )
+          : subtitle}
       </StyledSmallSubTitle>
       {changeSide ? (
         <></>
@@ -113,10 +128,10 @@ const GeneralBoxProject = ({
           {i6}
         </div>
       )}
-      {sideTitle === 'Projeto no Github' && changeSide ? (
+      {sideTitle === "Projeto no Github" && changeSide ? (
         <StyledSmallSubTitle
           onClick={() => {
-            window.open(projectLink, '_blank') // Corrigido aqui
+            window.open(projectLink, "_blank");
           }}
           textAlign="center"
           color="var(--grey7)"
@@ -127,7 +142,7 @@ const GeneralBoxProject = ({
         <></>
       )}
     </ProjBox>
-  )
-}
+  );
+};
 
-export default GeneralBoxProject
+export default GeneralBoxProject;
